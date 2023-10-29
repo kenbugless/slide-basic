2c4fd4b1abe46d35446d6d7f2936b4d7b2279030
kenbugless <kenbugless@gmail.com>
2023-10-29 09:13:45 +0900

    배열데이터를 통해 동적으로 panel ul 생성

--- js/main.js
+++ js/main.js
@@ -1,17 +1,13 @@
+const pics = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 const main = document.querySelector('main');
-const [panel, btns] = main.querySelectorAll('ul');
+//const [panel, btns] = main.querySelectorAll('ul');
 const speed = 3000;
 let evtBlock = false;
 
-/* 
-모션 중 이벤트핸들러 호출 방지 흐름 
-1. evtBlock 값을 false로 초기화한 뒤, 이벤트문 안쪽에 해당 값이 true 일때 return으로 중지처리
-2. 버튼 클릭 시, 무조건 evtBlock 전역변수의 값을 true로 변경 (처음에 한번만 이벤트를 호출하고 그 이후에는 동작이 막힘)
-3. new Anime의 callback함수로 evtBlock값을 false로 변경( 모션주엥는 이벤트함수 호출이 막혀있다가 모션이 끝나는 시점에 callback에 의해서 다시 전역변수를 이벤트 호출가능하돌고 변경)
-*/
+createDOM(main, pics);
+//console.log(Array.from(btns.children));
 
-console.log(Array.from(btns.children));
-
+/*
 Array.from(btns.children).forEach((btn, idx) => {
 	btn.addEventListener('click', (e) => {
 		if (e.target.classList.contains('on') || evtBlock) return;
@@ -19,6 +15,18 @@ Array.from(btns.children).forEach((btn, idx) => {
 		movePanel(panel, idx);
 	});
 });
+*/
+
+function createDOM(targetEl, arr) {
+	let tags = '';
+	const ul = document.createElement('ul');
+	ul.classList.add('panel');
+	arr.forEach((pic, idx) => {
+		tags += `<li style='background-image:url(img/${pic})'></li>`;
+	});
+	ul.innerHTML = tags;
+	targetEl.append(ul);
+}
 
 function activation(arr, idx) {
 	arr.forEach((el) => el.classList.remove('on'));
